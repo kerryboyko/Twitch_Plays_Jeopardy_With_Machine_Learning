c2651db33787640617d7114347c053c87d7f0da7
Brian Boyko <brian.boyko@gmail.com>
2020-12-10 07:01:07 +0000

    Added tests that pass

--- packages/server/src/Game/utils/createBoard.spec.ts
+++ packages/server/src/Game/utils/createBoard.spec.ts
@@ -1,21 +1,29 @@
+import randomSeed from "random-seed";
+import invert from "lodash/invert";
 import testBoard2 from "../../db/services/getClues/mocks/testBoard2.json";
 import { ClueCategory } from "../../types";
 import createBoard from "./createBoard";
 
+const rand = randomSeed.create("test");
+
 const tb2: ClueCategory[] = (testBoard2 as ClueCategory[]).slice(0, 6);
 
 test("createBoard", () => {
-  const board = createBoard(tb2);
+  const board = createBoard(tb2, rand, true);
+  const expectedLookup = {
+    dawn: 5,
+    europe: 3,
+    international: 0,
+    make: 1,
+    "one-word": 4,
+    twitters: 2,
+  };
   expect(board).toEqual({
-    clueSet: tb2,
-    lookup: {
-      dawn: 5,
-      europe: 3,
-      international: 0,
-      make: 1,
-      "one-word": 4,
-      twitters: 2,
-    },
+    clueSet: tb2.map((cat, index) => ({
+      ...cat,
+      key: invert(expectedLookup)[index.toString()],
+    })),
+    lookup: expectedLookup,
   });
   expect(board.clueSet[board.lookup.dawn].category).toBe("dawn");
   expect(board.clueSet[board.lookup.europe].category).toBe("europe");
--- packages/server/src/Game/utils/genSeedString.ts
+++ packages/server/src/Game/utils/genSeedString.ts
@@ -2,5 +2,4 @@ export const genSeedString = (): string => {
   return Math.random().toString(36).substring(4);
 };
 
-
 export default genSeedString;
