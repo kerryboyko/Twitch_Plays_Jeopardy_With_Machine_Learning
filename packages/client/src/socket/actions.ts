fce89a71f1bd9e18046e67d93ee41e1afc3d2d47
Brian Boyko <brian.boyko@gmail.com>
2020-12-23 00:21:10 +0000

    more sockets

--- packages/client/src/socket/actions.ts
+++ packages/client/src/socket/actions.ts
@@ -3,12 +3,27 @@ import { wsClient } from "@jeopardai/server/src/sockets/commands";
 import { socket } from "./socket";
 
 const startGame = (seed?: string): void => {
+  console.log(wsClient.START_GAME, seed);
   socket.emit(wsClient.START_GAME, seed);
 };
 
-const registerPlayer = (twitchId: string): void => {
-  console.log(wsClient.REGISTER_PLAYER, twitchId);
-  socket.emit(wsClient.REGISTER_PLAYER, twitchId);
+export const registerPlayer = (twitchId: string): void => {
+  console.log(wsClient.REGISTER_PLAYER, { twitchId });
+  socket.emit(wsClient.REGISTER_PLAYER, { twitchId });
 };
 
-export default { startGame, registerPlayer };
+export const provideAnswer = (twitchId: string, answer: string): void => {
+  console.log(wsClient.PROVIDE_ANSWER, { twitchId, answer });
+  socket.emit(wsClient.PROVIDE_ANSWER, { twitchId, answer });
+};
+
+export const selectClue = (
+  twitchId: string,
+  category: string,
+  value: number
+): void => {
+  console.log(wsClient.PROVIDE_ANSWER, { twitchId, category, value });
+  socket.emit(wsClient.PROVIDE_ANSWER, { twitchId, category, value });
+};
+
+export default { startGame, registerPlayer, provideAnswer, selectClue };
