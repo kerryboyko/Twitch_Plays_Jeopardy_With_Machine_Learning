4bd92bcda4277815f9f04619c79956ac0d7fcdf2
Brian Boyko <brian.boyko@gmail.com>
2020-12-26 00:27:45 +0000

    Starting to get it

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -7,6 +7,11 @@
   <register-player v-if="!store.state.user.connected" @login="handleLogin" />
   <button @click="startGame">Start Game</button>
   <answer-timer /> -->
+  <clue-display
+    :category="test.category"
+    :value="test.value"
+    :question="test.question"
+  />
   <answer-input />
 </template>
 
@@ -19,6 +24,7 @@ import socketActions from "./socket/actions";
 import SocketComponent from "./socket/SocketComponent.vue";
 import AnswerTimer from "./components/AnswerTimer.vue";
 import AnswerInput from "./components/AnswerInput.vue";
+import ClueDisplay from "./components/ClueDisplay.vue";
 
 export default defineComponent({
   name: "App",
@@ -28,17 +34,23 @@ export default defineComponent({
     // GameTimer,
     // AnswerTimer,
     AnswerInput,
+    ClueDisplay,
   },
   setup() {
     const store = useStore();
-
+    const test = {
+      question:
+        "Dexy's Midnight Runners No. 1 hit heard here in a version by ska band Save Ferris:",
+      value: 800,
+      category: "under the covers",
+    };
     const handleLogin = (twitchId: string) => {
       if (twitchId !== "") {
         socketActions.registerPlayer(twitchId);
       }
     };
 
-    return { store, startGame: socketActions.startGame, handleLogin };
+    return { store, startGame: socketActions.startGame, handleLogin, test };
   },
 });
 </script>
---
+++ packages/client/src/components/ClueDisplay.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="clue">
+    <div class="clue__internals">
+      <div class="clue__category">{{ category }} - ${{ value }}</div>
+      <div class="clue__question">
+        {{ question }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ClueDisplay",
+  props: ["category", "value", "question"],
+});
+</script>
+
+<style lang="scss" scoped>
+.clue {
+  font-family: "Korinna", sans-serif;
+  text-transform: uppercase;
+  height: 360px;
+  width: 100%;
+  padding: 10px 50px 50px 50px;
+  background-image: url("/img/clue-background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #ffffff;
+  font-size: 25px;
+  text-shadow: 3px 4px 2px #000000;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: center;
+  align-items: center;
+  &__internals {
+    width: 100%;
+    max-width: 600px;
+  }
+  &__category {
+    height: 40px;
+    font-size: 20px;
+    font-family: Bebas Neue;
+    color: #e7ddcb;
+  }
+  &__question {
+    display: flex;
+    height: 290px;
+    letter-spacing: 1px;
+    text-align: center;
+    align-items: center;
+  }
+}
+</style>
